3bc86b56fcdd65a0856fa46260bbd5cd8659e787
Igor Radovanov <igor@radovanovigor.com>
2021-11-08 10:29:46 +0100

    Re-rendering classic

--- part1/component-state/src/App.js
+++ part1/component-state/src/App.js
@@ -1,31 +1,9 @@
 import './App.css';
 
-function Hello ({name, age}){
-  const bornYear = () => new Date().getFullYear() - age;
-
+function App(props) {
+  const {counter} = props;  
   return (
-    <div>
-      <p>
-        Hello {name}, you are {age} old :D
-      </p>
-      <p>
-        You are born in {bornYear()} 
-      </p>
-    </div>
-  )
-}
-
-
-function App() {
-  const name = 'Peter'
-  const age = 20
-  
-  return (
-      <div>
-        <h1>Greetings!</h1>
-        <Hello name='Maya' age={5+25}></Hello>
-        <Hello name={name} age={25}></Hello>
-      </div>
+    <div>{counter}</div>
   );
 }
 
