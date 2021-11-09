ae159e486d6bdb9e8b5bb4bc6aa848331d9afa53
Igor Radovanov <igor@radovanovigor.com>
2021-11-09 11:36:01 +0100

    State

--- part1/component-state/src/App.js
+++ part1/component-state/src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
+import React, {useState} from "react";
 
-function App(props) {
-  const {counter} = props;  
-  return (
-    <div>{counter}</div>
-  );
+const App = () => {
+
+  const [counter, setCounter] = useState(0)
+
+  setTimeout(() => setCounter (counter +1 ), 1000)
+  return(
+    <div>
+      {counter}
+    </div>
+  )
 }
 
 export default App;
--- part1/component-state/src/index.js
+++ part1/component-state/src/index.js
@@ -1,21 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from "react-dom"
+import App from './App'
 
-let counter = 1;
-
-const refresh = () => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <App counter={counter} />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-}
-
-setInterval(() => {
-  refresh();
-  counter += 1
-}, 1000)
+ReactDom.render(<App/>, document.getElementById('root'))
