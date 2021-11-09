0d2866625ea57596e51dc67d26f3977eca263d52
Igor Radovanov <igor@radovanovigor.com>
2021-11-09 13:29:49 +0100

    Shorter func call

--- part1/component-state/src/App.js
+++ part1/component-state/src/App.js
@@ -1,17 +1,39 @@
 import React, {useState} from "react";
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
+    const [clicks, setClicks] = useState({left: 0, right:0})
 
-  return(
-    <div>
-      {left}
-      <button onClick={() => setLeft(left + 1)}>left</button>
-      {right}
-      <button onClick={() => setRight(right +1)}>right</button>
-    </div>
-  )
+    // const handleLeftClick = () => {
+    //   const newClicks = {
+    //     left: clicks.left + 1,
+    //     right: clicks.right
+    //   }
+    //   setClicks(newClicks)
+    // }
+
+    const handleLeftClick = () => {setClicks({...clicks, left: clicks.left +1})}
+
+    // const handleRightClick = () => {
+    //   const newClicks = {
+    //     left: clicks.left,
+    //     right: clicks.right + 1
+    //   }
+    //   setClicks(newClicks)
+    // }
+
+    const handleRightClick = () => {setClicks({...clicks, right: clicks.right +1})}
+
+
+//return
+
+return(
+  <div>
+    {clicks.left}
+    <button onClick={handleLeftClick}>left</button>
+    <button onClick={handleRightClick}>right</button>
+    {clicks.right}
+  </div>
+)
+
 }
-
 export default App;
